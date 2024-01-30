940b43bc99d4ba011b12afb41ee575a075135907
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-01-30 22:47:07 +0100

    updated callback function addWand

--- magic-wand-api/controllers/wand.js
+++ magic-wand-api/controllers/wand.js
@@ -1,9 +1,43 @@
+const WandModel = require("../models/wandModel");
+const mongoose = require("mongoose");
+
 exports.hello = (req, res) => {
   res.send("hello!");
 };
 
 exports.addWand = async (req, res) => {
-  res.send(req.body);
+  const { flexibility, owner, length, wood } = req.body;
+
+  console.log(owner._id);
+
+  const wand = new WandModel({
+    flexibility,
+    owner,
+    length,
+    wood,
+  });
+
+  try {
+    if (!flexibility || !owner || !length || !wood) {
+      return res.status(400).json("All field are required!");
+    }
+    await wand.save();
+    res.status(201).json({ message: "Wand created", ...wand });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res
+        .status(400)
+        .json({ message: "Validation error", error: error.message });
+    }
+
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ message: "Validation error", error: error.message });
+    }
+
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 exports.getWands = async (req, res) => {
