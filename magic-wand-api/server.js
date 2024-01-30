949cf500f4bb9bc9427fe4e960e8ea6f93cf46e1
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-01-30 14:03:52 +0100

    adding port and middleware to server.js

--- magic-wand-api/server.js
+++ magic-wand-api/server.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT:${PORT}`);
+});
