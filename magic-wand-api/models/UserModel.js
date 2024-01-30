5f58d99fb05a67526d9ff4bf1a601617840d223b
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-01-30 22:45:50 +0100

    added timestamps

--- magic-wand-api/models/UserModel.js
+++ magic-wand-api/models/UserModel.js
@@ -1,22 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    minLength: 3,
-    maxLength: 23,
-    unique: true,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      minLength: 3,
+      maxLength: 23,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minLength: 3,
+      maxLength: 23,
+      required: true,
+      trim: true,
+    },
   },
-  password: {
-    type: String,
-    minLength: 3,
-    maxLength: 23,
-    required: true,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
 const UserModel = model("User", UserSchema);
 
--- magic-wand-api/models/WandModel.js
+++ magic-wand-api/models/WandModel.js
@@ -1,31 +1,35 @@
 const { Schema, model } = require("mongoose");
-const UserModel = require("./UserModel");
+const mongoose = require("mongoose");
 
 const WoodEnum = ["Alder", "Acacia", "Apple", "Ash", "Blackthorn", "Cherry"];
 
-const WandSchema = new Schema({
-  flexibility: {
-    type: String,
-    maxLength: 50,
-    trim: true,
-    required: true,
+const WandSchema = new Schema(
+  {
+    flexibility: {
+      type: String,
+      maxLength: 50,
+      trim: true,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    length: {
+      type: Number,
+      min: 1,
+      required: true,
+    },
+    wood: {
+      type: String,
+      enum: WoodEnum,
+      trim: true,
+      required: true,
+    },
   },
-  owner: {
-    type: UserModel.schema,
-    required: true,
-  },
-  length: {
-    type: Number,
-    min: 1,
-    required: true,
-  },
-  wood: {
-    type: String,
-    enum: WoodEnum,
-    trim: true,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const WandModel = model("Wand", WandSchema);
 
