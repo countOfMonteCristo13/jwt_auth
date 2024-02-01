a67699a9de8781fc67af38b716771c8237f576f0
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-02-01 16:33:19 +0100

    deleting test hello route

--- magic-wand-api/routes/wandRoutes.js
+++ magic-wand-api/routes/wandRoutes.js
@@ -1,13 +1,7 @@
-const {
-  hello,
-  getWands,
-  getWand,
-  addWand,
-} = require("../controllers/wandController");
+const { getWands, getWand, addWand } = require("../controllers/wandController");
 
 const router = require("express").Router();
 
-router.get("/", hello);
 router.post("/add-wand", addWand);
 router.get("/get-wands", getWands);
 router.get("/get-wands/:id", getWand);
