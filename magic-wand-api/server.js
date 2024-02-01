c5f79fba1feb1cf5357cbcf38cd4235779083eed
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-02-01 16:28:46 +0100

    rearranging functions and variables in server.js

--- magic-wand-api/server.js
+++ magic-wand-api/server.js
@@ -2,14 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const { db } = require("./db/db");
 const routes = require("./routes/index");
+const app = express();
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-app.use(express.json());
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -17,8 +14,8 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use(express.json());
 app.use(cors(corsOptions));
-
 app.use("/api", routes);
 
 app.listen(PORT, () => {
