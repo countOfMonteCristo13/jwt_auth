777e6ef0690bc72e5ec42d85a4208c8a82b0cde3
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-02-01 06:45:49 +0100

    added logOut function and navigations to other routes

--- magic-wand/src/containers/WandsList/WandsList.tsx
+++ magic-wand/src/containers/WandsList/WandsList.tsx
@@ -4,21 +4,30 @@ import TableContainerHeading from "../../components/TableContainerHeading/TableC
 import WandsTable from "../../components/WandsTable/WandsTable";
 import useWands from "../../hooks/useWands";
 import { TableContainer } from "../../styles/TableLayouts";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthProvider";
 
 type WandsListProps = {
   authenticated?: boolean;
 };
 
 const WandsList: React.FC<WandsListProps> = ({ authenticated = false }) => {
+  const { setAuth } = useContext(AuthContext);
   const { wands, isLoading, error } = useWands();
   const navigate = useNavigate();
-  // console.log(wands);
+
+  const handleLogOut = () => {
+    setAuth({});
+    navigate("/");
+  };
 
   return (
     <TableContainer>
       <TableContainerHeading
         authenticated={authenticated}
         createNewWand={() => navigate("/add-wand")}
+        logIn={() => navigate("/login")}
+        logOut={handleLogOut}
       />
       {authenticated ? (
         <AuthWandsTable wands={wands} error={error} isLoading={isLoading} />
