49fcdaa4af726f59a064a56e965011ed9617489f
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-02-01 06:42:19 +0100

    updating routes based on auth

--- magic-wand/src/App.tsx
+++ magic-wand/src/App.tsx
@@ -1,24 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import WandsList from "./containers/WandsList/WandsList";
 import CreateWand from "./containers/CreateWand/CreateWand";
 import Login from "./containers/Login/Login";
 import WandDetails from "./containers/WandDetails/WandDetails";
 import { StyledMain } from "./styles/GlobalLayouts";
+import { useContext } from "react";
+import AuthContext from "./context/AuthProvider";
+import RequireAuth from "./routes/RequireAuth";
 
 function App() {
+  const { auth } = useContext(AuthContext);
+  console.log(auth);
   return (
     <StyledMain>
       <Routes>
-        <Route path="/" element={<WandsList authenticated={true} />} />
-        <Route path="/:id" element={<WandDetails />} />
-        <Route path="/add-wand" element={<CreateWand />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/wands" element={<WandsList authenticated={false} />} />
         <Route path="/login" element={<Login />} />
+
+        <Route element={<RequireAuth />}>
+          <Route path="/auth/wands/:id" element={<WandDetails />} />
+          <Route path="/add-wand" element={<CreateWand />} />
+          <Route
+            path="/auth/wands"
+            element={<WandsList authenticated={true} />}
+          />
+        </Route>
+
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
     </StyledMain>
   );
 }
 
 export default App;
-
-// "65b96c975ad0325443a53d47"
