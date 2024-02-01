706f8d298091cc95049be9f9a7d78ffb5bf445c7
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-02-01 06:44:33 +0100

    added file for authenticated routes

---
+++ magic-wand/src/routes/RequireAuth.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import AuthContext from "../context/AuthProvider";
+
+const RequireAuth = () => {
+  const { auth } = useContext(AuthContext);
+
+  return auth?.accessToken ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default RequireAuth;
